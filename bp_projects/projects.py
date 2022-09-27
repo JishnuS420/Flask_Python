b00c1914c3241598c206cbae0608cc77eba7ef5a
JishnuS420 <broburn123@gmail.com>
2022-09-26 19:13:08 -0700

    mods

--- bp_projects/projects.py
+++ bp_projects/projects.py
@@ -21,3 +21,16 @@ def walruses():
 @app_projects.route('/hawkers/')
 def hawkers():
     return render_template("hawkers.html")
+
+from flask import Flask, request, render_template
+import re
+app = Flask(__name__)
+
+@app.route("/",methods=['GET', 'POST'] )
+def main():
+    color = "FFFFFF"
+    new_color = request.form.get('color', '')
+    if re.search(r'^[0-9A-F]{6}$', new_color):
+        color = new_color
+
+    return render_template('main.html', color = color)
